85893e5f076a13f2f6cf414dd563ee4049d9feb9
Dave Chen <dchen@mail.nih.gov>
2023-01-23 15:18:10 -0500

    Fixes for flake8

--- utils/myspell.py
+++ utils/myspell.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import sys
 from enchant.checker import SpellChecker
 from enchant.tokenize import EmailFilter, URLFilter
 from enchant import DictWithPWL
@@ -17,9 +18,13 @@ def myspell(fname):
     for x in fp:
         spell_checker.set_text(x)
         for error in spell_checker:
-            print("Error:", error.word, "(line", lc, ")")
+            print("Error:", error.word, "(line", lc, ")", file=sys.stderr)
         lc = lc+1
 
 
 if __name__ == '__main__':
-    myspell('/Users/dchen/SimpleITK/Code/Common/include/sitkImage.h')
+    if len(sys.argv) > 1:
+        for x in sys.argv[1:]:
+            myspell(x)
+    else:
+        print("Usage: myspell.py file1 ... fileN")
